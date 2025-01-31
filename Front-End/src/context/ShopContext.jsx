636591615d12d7ba541e6228c6cc913aad3693e4
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-01-31 23:50:10 +0100

    cart function

--- Front-End/src/context/ShopContext.jsx
+++ Front-End/src/context/ShopContext.jsx
@@ -1,5 +1,5 @@
 // ShopContextProvider manages the shop's context, providing access to products, currency, and delivery fee
-import { createContext } from "react"
+import { createContext, useEffect, useState } from "react"
 import { products } from "../assets/assets"
 
 export const ShopContext = createContext();
@@ -7,9 +7,43 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
 	const currency = 'TND';
 	const delivery_fee = 10;
+	const [cartItems,setCartItems] = useState({});
+
+	const addToCart = async (itemId) => {
+		let cartData = structuredClone(cartItems);
+
+		if (cartData[itemId]) {
+			if (cartData[itemId]) {
+				cartData[itemId] += 1;
+			}
+			else {
+				cartData[itemId] = 1;
+			}
+		}
+		else {
+			cartData[itemId] = {};
+			cartData[itemId] = 1;
+		}
+		setCartItems(cartData);
+	}
+
+	const getCartCount = () => {
+		let totalCount = 0;
+		for(const items in cartItems){
+			try {
+				if (cartItems[items] > 0) {
+					totalCount += cartItems[items];
+				}
+			} catch (error) {
+
+			}
+		}
+		return totalCount;
+	}
 
 	const value = {
-		products, currency, delivery_fee
+		products, currency, delivery_fee,
+		cartItems, addToCart, getCartCount
 	}
 
 	return (
