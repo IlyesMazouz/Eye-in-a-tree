cec227c34df6e0257df833b0267e7c392bed2107
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-02-01 21:28:51 +0100

    login page

--- Front-End/src/pages/Login.jsx
+++ Front-End/src/pages/Login.jsx
@@ -1,12 +1,34 @@
-// work in progress
-import React from 'react'
+import React, { useState } from 'react'
+import '../styles/Login.css'
 
 const Login = () => {
+
+  	const [currentState, setCurrentState] = useState('Sign up');
+	const onSubmitHandler = async (event) => {
+		event.preventDefault();
+	}
+
   return (
-	<div>
-	  
-	</div>
-  )
+    <form  onSubmit={onSubmitHandler} className="login-form">
+      <div className="heading-container">
+        <p className="heading-text">{currentState}</p>
+        <hr className="separator" />
+      </div>
+	  	{currentState === 'Login' ? '' : <input type="text" className="input-field-login" placeholder="Name" required />}
+		<input type="email" className="input-field-login" placeholder="Email" required />
+		<input type="password" className="input-field-login" placeholder="Password" required />
+		<div className="login-footer">
+  <p>Forgot your password?</p>
+  	{
+    	currentState === 'Login'
+    	? <p onClick={() => setCurrentState('Sign Up')}>Create account</p>
+    	: <p onClick={() => setCurrentState('Login')}>Login Here</p>
+  	}
+		</div>
+		<button className="login-btn">{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+
+    </form>
+  );
 }
 
-export default Login
+export default Login;
