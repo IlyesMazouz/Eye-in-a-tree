55a7378bc79ed95ee915950993e9a4adb44558d3
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-02-07 21:21:42 +0100

    adding routes

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import SideBar from './components/SideBar'
+import './styles/App.css'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
-    <>
+    <div className="app-container">
       <NavBar />
-    </>
-  )
-}
+      <hr />
+      <div className="content">
+        <SideBar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/add" element={<Add />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
