26a00e3f2113f5cf12094d8e040e6730a673740c
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-02-09 13:54:28 +0100

    adding set token

--- Admin/src/components/NavBar.jsx
+++ Admin/src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { assets } from '../assets/assets'
 import '../styles/NavBar.css'
 
-const NavBar = () => {
+const NavBar = ({setToken}) => {
   return (
     <div className="navbar">
       <img className="logo" src={assets.logo} alt="Logo" />
-      <button className="logout-button">Logout</button>
+      <button onClick={()=>setToken('')} className="logout-button">Logout</button>
     </div>
   );
 };
