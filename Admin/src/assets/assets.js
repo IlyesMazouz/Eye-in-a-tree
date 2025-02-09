67523ec3946ffb09aed475d7cf9b8ef961a3cc38
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-02-09 18:15:28 +0100

    adding upload icone

--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -2,10 +2,13 @@ import logoPNG from './logo.PNG'
 import addPNG from './add.PNG'
 import listPNG from './list.PNG'
 import ordersPNG from './orders.PNG'
+import uploadPNG from './upload.PNG'
+
 
 export const assets = {
 	logo: logoPNG,
 	add_icone: addPNG,
 	list_icone: listPNG,
 	orders_icone: ordersPNG,
+	upload_icone: uploadPNG,
 }
