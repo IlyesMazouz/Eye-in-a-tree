a40c70fc4d666ffc2ca3f5e518195a0f297ceb0f
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-02-10 19:57:28 +0100

    creating the rest of api for cart

--- Back-End/controllers/cartController.js
+++ Back-End/controllers/cartController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel.js";
+import userModel from "../models/userModel.js"
 
 
 const addToCart = async (req,res) => {
@@ -33,11 +33,36 @@ try {
 }
 
 const updateCart = async (req,res) => {
-	
+	try {
+		
+		const {userId,itemId,quantity} = req.body
+
+		const userData = await userModel.findById(userId)
+		let cartData = await userData.cartData;
+
+		cartData[itemId] = quantity
+
+		await userModel.findByIdAndUpdate(userId, {cartData})
+		res.json({success: true, message: "Cart Updated"})		
+
+	} catch (error) {
+		console.log(error)
+		res.json({success: false, message: error.message})
+	}
 }
 
 const getUserCart = async (req,res) => {
-	
+	try {
+		const {userId} = req.body;
+		const userData = await userModel.findById(userId);
+		let cartData = userData.cartData;
+
+		res.json({success: true, cartData});
+	} catch (error) {
+		console.log(error);
+		res.json({success: false, message: error.message});
+	}
 }
+
 
 export {addToCart,updateCart,getUserCart};
