c24dc43e9c0f6a668d94de9f693db9976217cce4
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-02-10 23:34:55 +0100

    linking place order api with front end

--- Front-End/src/pages/PlaceOrder.jsx
+++ Front-End/src/pages/PlaceOrder.jsx
@@ -1,31 +1,97 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Title from '../components/Title'
 import '../styles/PlaceOrder.css'
 import CartTotal from '../components/CartTotal'
 import { ShopContext } from '../context/ShopContext'
+import { data } from 'react-router-dom'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const PlaceOrder = () => {
 
-	const {navigate} = useContext(ShopContext);
+	const [method,setMethod] = useState('cod');
+	const {navigate,backendUrl,token,cartItems,setCartItems,getCartAmount,delivery_fee,products} = useContext(ShopContext);
+	const [fromData,setFromData] = useState({
+		firstName:'',
+		lastName:'',
+		email:'',
+		street:'',
+		city:'',
+		state:'',
+		zipcode:'',
+		phone:''
+	})
+
+	const onChangeHandler = (event) => {
+		const name = event.target.name
+		const value = event.target.value
+
+		setFromData(data => ({...data,[name]:value}))
+	}
+	
+	const onSubmitHandler = async (event) => {
+		event.preventDefault();
+		try {
+			let orderItems = [];
+	
+			for (const productId in cartItems) {
+				if (cartItems[productId] > 0) {
+					const itemInfo = structuredClone(products.find(product => product._id === productId));
+					if (itemInfo) {
+						itemInfo.quantity = cartItems[productId];  
+						orderItems.push(itemInfo);
+					}
+				}
+			}
+	
+			let orderData = {
+				address: fromData,
+				items: orderItems,
+				amount: getCartAmount() + delivery_fee,
+				paymentMethod: method
+			}
+
+			switch(method) {
+				case 'cod':
+					const response = await axios.post(backendUrl + '/api/order/place',orderData,{headers:{token}})
+					console.log(response.data);
+					if (response.data.success) {
+						setCartItems({})
+						navigate('/orders')
+					} else {
+						toast.error(response.data.message)
+					}
+					break;
+
+					default:
+						break;
+			}
+
+		} catch (error) {
+			console.error(error)
+			toast.error(error.message)
+		}
+	};
+	
 
   return (
-    <div className="place-order-container">
+    <form onSubmit={onSubmitHandler} className="place-order-container">
       <div className="delivery-info">
         <div className="title-container">
           <Title text1={'DELIVERY'} text2={'INFORMATION'} />
         </div>
         <div className="input-container">
-  			<input className="input-field" type="text" placeholder="First name" />
-  			<input className="input-field" type="text" placeholder="Last name" />
+  			<input required onChange={onChangeHandler} name='firstName' value={fromData.firstName} className="input-field" type="text" placeholder="First name" />
+  			<input required onChange={onChangeHandler} name='lastName' value={fromData.lastName} className="input-field" type="text" placeholder="Last name" />
 		</div>
-		<input className="input-field-other" type="email" placeholder="Email adress" />
-		<input className="input-field-other" type="text" placeholder="Street" />
+		<input required onChange={onChangeHandler} name='email' value={fromData.email} className="input-field-other" type="email" placeholder="Email adress" />
+		<input required onChange={onChangeHandler} name='street' value={fromData.street} className="input-field-other" type="text" placeholder="Street" />
 		<div className="input-container">
-  			<input className="input-field" type="text" placeholder="City" />
-  			<input className="input-field" type="text" placeholder="State" />
-			<input className="input-field-zip" type="number" placeholder="Zip Code" />
+  			<input required onChange={onChangeHandler} name='city' value={fromData.city} className="input-field" type="text" placeholder="City" />
+  			<input required onChange={onChangeHandler} name='state' value={fromData.state} className="input-field" type="text" placeholder="State" />
+			<input required onChange={onChangeHandler} name='zipcode' value={fromData.zipcode} className="input-field-zip" type="number" placeholder="Zip Code" />
 		</div>
-		<input className="input-field-other" type="number" placeholder="Phone" />
+		<input required onChange={onChangeHandler} name='phone' value={fromData.phone} className="input-field-other" type="number" placeholder="Phone" />
       </div>
 	  <div className="cart-total-wrapper">
   		<div className="cart-total-box">
@@ -34,16 +100,16 @@ const PlaceOrder = () => {
 		  <div className="payment-method">
   <Title text1={'PAYMENT'} text2={'METHOD'} />
   <div className="payment-options">
-    <div className="payment-option">
+    <div onClick={()=>setMethod('cod')} className="payment-option">
       <p className="payment-text">CASH ON DELIVERY</p>
     </div>
   </div>
-  <div className="place-order-container-button">
-  <button onClick={()=>navigate('/orders')} className="place-order-button">PLACE ORDER</button>
+  <div  className="place-order-container-button">
+  <button type='submit' className="place-order-button">PLACE ORDER</button>
 </div>
 </div>
 </div>
-</div>
+</form>
   );
 };
 
