634dbe2c619b236bda72495ba9c91db35fd0d0bb
IlyesMazouz <ilyesmazouzz@gmail.com>
2025-02-11 13:52:19 +0100

    creating update order status function

--- Back-End/controllers/orderController.js
+++ Back-End/controllers/orderController.js
@@ -33,7 +33,14 @@ const placeOrder = async (req,res) => {
 }
 
 const allOrders = async (req,res) => {
+	try {
+		const orders = await orderModel.find({})
+		res.json({success:true,orders})
 
+	} catch (error) {
+		console.log(error);
+		res.json({success:false, message:error.message})
+	}
 }
 
 const userOrders = async (req,res) => {
@@ -51,7 +58,17 @@ const userOrders = async (req,res) => {
 }
 
 const updateStatus = async (req,res) => {
+	try {
+		
+		const {orderId, status} = req.body
 
+		await orderModel.findByIdAndUpdate(orderId, {status})
+		res.json({success:true,message:'Status Updated'})
+		
+	} catch (error) {
+		console.log(error);
+		res.json({success:false, message:error.message})
+	}
 }
 
 export {placeOrder,allOrders,userOrders,updateStatus}
